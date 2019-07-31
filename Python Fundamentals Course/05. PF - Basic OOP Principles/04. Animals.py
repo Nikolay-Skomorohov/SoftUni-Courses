da8fe7fbfc340de7bbafc7da761ab8229484a81f
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2019-07-31 19:24:28 +0300

    Update the solution.

--- Python Fundamentals Course/05. PF - Basic OOP Principles/04. Animals.py
+++ Python Fundamentals Course/05. PF - Basic OOP Principles/04. Animals.py
@@ -86,8 +86,50 @@ class Tomcat(Cat):
         self.__gender = "Male"
 
 
+def input_data():
+    animals_list = []
+    animal_type_input = input()
+    animal_specs_input = input().split()
+    while animal_type_input != "Beast!":
+        if animal_type_input == "Dog":
+            new_dog = Dog(animal_specs_input[0],
+                          int(animal_specs_input[1]),
+                          animal_specs_input[2])
+            animals_list.append(new_dog)
+        elif animal_type_input == "Frog":
+            new_frog = Frog(animal_specs_input[0],
+                            int(animal_specs_input[1]),
+                            animal_specs_input[2])
+            animals_list.append(new_frog)
+        elif animal_type_input == "Cat":
+            new_cat = Frog(animal_specs_input[0],
+                           int(animal_specs_input[1]),
+                           animal_specs_input[2])
+            animals_list.append(new_cat)
+        elif animal_type_input == "Kitten":
+            new_kitten = Frog(animal_specs_input[0],
+                              int(animal_specs_input[1]),
+                              animal_specs_input[2])
+            animals_list.append(new_kitten)
+        elif animal_type_input == "Tomcat":
+            new_tomcat = Frog(animal_specs_input[0],
+                              int(animal_specs_input[1]),
+                              animal_specs_input[2])
+            animals_list.append(new_tomcat)
+        else:
+            raise Exception("Invalid animal type input")
+
+        animal_type_input = input()
+        animal_specs_input = input().split()
+    return animals_list
+
+def print_data():
+
+
+
 def main():
-    pass
+    to_print_list = input_data()
+    print_data(to_print_list)
 
 
 if __name__ == "__main__":
