35bb35a06cb0b7cc81c082b17a31486e82d7b50b
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2019-08-29 21:01:37 +0300

    Updated the partial solution.

--- Python Fundamentals Course/07. PF - Regular Expressions/13. Commits.py
+++ Python Fundamentals Course/07. PF - Regular Expressions/13. Commits.py
@@ -1,8 +1,8 @@
 import re
 
-pattern = r"(https://github\.com/([A-Za-z0-9\-]+)/([a-zA-Z\-\_]+)/commit/([0-9a-f]{40}),([^(\n]+),([0-9]+),([0-9]+))"
+pattern = r"https://github\.com/(?P<user>[A-Za-z0-9\-]+)/(?P<repo>[a-zA-Z-_]+)/commit/(?P<hash>[0-9a-f]{40}),(?P<message>[^(\n]+),(?P<add>[0-9]+),(?P<del>[0-9]+)"
 data_dict = {}
-
+total_dict = {}
 while True:
     command = input()
     if command == "git push":
@@ -11,14 +11,20 @@ while True:
         string = re.fullmatch(pattern, command)
         if string.group('user') not in data_dict.keys():
             data_dict[string.group('user')] = {}
+            total_dict[string.group('user')] = {}
         if string.group('repo') not in data_dict[string.group('user')].keys():
             data_dict[string.group('user')][string.group('repo')] = {}
+            total_dict[string.group('user')][string.group('repo')] = {'add': 0, 'del': 0}
         data_dict[string.group('user')][string.group('repo')][string.group('hash')] = f"commit {string.group('hash')}: {string.group('message')} ({string.group('add')} additions, {string.group('del')} deletions)"
+        total_dict[string.group('user')][string.group('repo')]['add'] += int(string.group('add'))
+        total_dict[string.group('user')][string.group('repo')]['del'] += int(string.group('del'))
     else:
         pass
 
-for usr in data_dict.keys():
+for usr, val1 in sorted(data_dict.items()):
     print(f"{usr}:")
-    for rep in usr.keys():
-
-    print(f"  {us}:")
+    for rep in sorted(val1.keys()):
+        print(f"  {rep}:")
+        for val2 in data_dict[usr][rep].values():
+            print(f"    {val2}")
+        print(f"    Total: {total_dict[usr][rep]['add']} additions, {total_dict[usr][rep]['del']} deletions")
