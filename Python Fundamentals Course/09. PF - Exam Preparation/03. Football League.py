7013f90d172a61d2a56dbc1bac903c9d517498a2
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2019-09-02 20:00:28 +0300

    Add partial exercise solution.

--- Python Fundamentals Course/09. PF - Exam Preparation/03. Football League.py
+++ Python Fundamentals Course/09. PF - Exam Preparation/03. Football League.py
@@ -1,50 +1,57 @@
 import re
 
-decrypt_key = input()
-team_pattern = rf'[{decrypt_key}].*[{decrypt_key}]'
-line_of_code = input().split()
-teams_dict_points = {}
-teams_dict_goals = {}
 
-while line_of_code[0] != 'final':
-    score = line_of_code[2].split(':')
+def decrypt_command(team1: str, team2: str, key: str):
+    new_command = []
+    pattern = rf"(.*?)(?P<key1>[{key}]+)(?P<name>.+?)(?P<key2>[{key}]+)(.*)"
+    team_1 = re.search(pattern, team1).group('name')
+    new_command.append(team_1[::-1])
+    team_2 = re.search(pattern, team2).group('name')
+    new_command.append(team_2[::-1])
+    return new_command
 
-    team_1 = re.search(team_pattern, line_of_code[0]).group()
-    team_1 = team_1.replace(decrypt_key, "")
-    team_1 = team_1[::-1].upper()
-    if team_1 not in teams_dict_points.keys():
-        teams_dict_points[team_1] = 0
-        teams_dict_goals[team_1] = 0
-    if int(score[0]) > int(score[1]):
-        teams_dict_points[team_1] += 3
-    elif score[0] == score[1]:
-        teams_dict_points[team_1] += 1
-    teams_dict_goals[team_1] += int(score[0])
 
-    team_2 = re.search(team_pattern, line_of_code[1]).group()
-    team_2 = team_2.replace(decrypt_key, "")
-    team_2 = team_2[::-1].upper()
-    if team_2 not in teams_dict_points.keys():
-        teams_dict_points[team_2] = 0
-        teams_dict_goals[team_2] = 0
-    if int(score[0]) < int(score[1]):
-        teams_dict_points[team_2] += 3
-    elif score[0] == score[1]:
-        teams_dict_points[team_2] += 1
-    teams_dict_goals[team_2] += int(score[1])
+def print_standing(data_dict: dict):
+    print("League standings:")
+    # data_dict_sorted = sorted(data_dict.items(), key=lambda x: x[1])
+    for k, v in enumerate(sorted(data_dict.items(), key=lambda x: x[1]), 1):
+        print(f"{k}. {v[0].upper()} {v[1]}")
 
-    line_of_code = input().split()
 
-print('League standings:')
-counter = 1
-for k, v in sorted(teams_dict_points.items(), key=lambda x: x[1], reverse=True):
-    print(f"{counter}. {k} {v}")
-    counter += 1
+def print_top_goals(data_dict: dict):
+    count = 1
+    print("Top 3 scored goals:")
+    for team in sorted(data_dict, key=lambda x: x[1]):
+        print(f"- {team[0].upper()} -> {team[1]}")
+        count += 1
+        if count > 3:
+            break
 
-print('Top 3 scored goals:')
-counter = 1
-for k, v in sorted(teams_dict_goals.items(), key=lambda x: x[1], reverse=True):
-    print(f"- {k} -> {v}")
-    counter += 1
-    if counter > 3:
-        break
+
+def main():
+    encryption_key = input()
+    data_dict = {}
+
+    while True:
+        command = input().split()
+        if command[0] == 'final':
+            break
+        else:
+            team_list = decrypt_command(command[0], command[1], encryption_key)
+            match_result = list(map(int, command[2].split(':')))
+            for team in team_list:
+                if team not in data_dict.keys():
+                    data_dict[team] = {'points': 0, 'goals': 0}
+                if match_result[0] > match_result[1]:
+                    data_dict[team]['points'] += 3
+                elif match_result[0] == match_result[1]:
+                    data_dict[team]['points'] += 1
+            data_dict[team_list[0]]['goals'] += match_result[0]
+            data_dict[team_list[1]]['goals'] += match_result[1]
+
+    print_standing(data_dict)
+    print_top_goals(data_dict)
+
+
+if __name__ == '__main__':
+    main()
