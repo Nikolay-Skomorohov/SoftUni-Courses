cfe3d51043148ef7f712bd20bb95773024fce509
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2019-09-05 21:05:12 +0300

    Add partial exercise solution.

---
+++ Python Fundamentals Course/10. PF - Exam Preparations 2/01. Date Estimation.py
@@ -0,0 +1,11 @@
+read_input = list(map(int, input().split('-')))
+
+year_today = 2018
+month_today = 8
+day_today = 26
+
+year_input = read_input[0]
+month_input = read_input[1]
+day_input = read_input[2]
+
+
