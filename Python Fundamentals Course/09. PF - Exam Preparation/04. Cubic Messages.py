a564cce8b543d1c022576da06e207d839da530df
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2019-09-05 20:55:32 +0300

    Add full exercise solution.

--- Python Fundamentals Course/09. PF - Exam Preparation/04. Cubic Messages.py
+++ Python Fundamentals Course/09. PF - Exam Preparation/04. Cubic Messages.py
@@ -10,17 +10,28 @@ class Message:
         self.code = None
 
     def calculate_code(self):
+        verify_code = []
+        for letter in self.before:
+            try:
+                verify_code.append(self.text[int(letter)])
+            except:
+                verify_code.append(" ")
+        for char in self.after:
+            try:
+                verify_code.append(self.text[int(char)])
+            except:
+                verify_code.append(" ")
 
-        return
+        self.code = "".join(verify_code)
 
     def __str__(self):
-        return f"{self.text}=={self.code}"
+        return f"{self.text} == {self.code}"
 
 
 def check_input(message: str, number_of_chars: int):
-    pattern = r"(?P<before>[0-9]{1,})(?P<msg>[A-Za-z]{" + f"{number_of_chars}" + r"})(?P<after>[^A-Za-z]{1,})"
-    if re.match(pattern, message):
-        output = re.match(pattern, message)
+    pattern = r"(?P<before>[0-9]{1,})(?P<msg>[A-Za-z]{" + f"{number_of_chars}" + r"})(?P<after>[^A-Za-z]*)"
+    if re.fullmatch(pattern, message):
+        output = re.fullmatch(pattern, message)
         return Message(text=output.group('msg'),
                        length=number_of_chars,
                        before=output.group('before'),
@@ -31,7 +42,7 @@ def check_input(message: str, number_of_chars: int):
 
 def print_output(message_data: list):
     for item in message_data:
-        item.calulate_code()
+        item.calculate_code()
         print(item)
 
 
@@ -39,12 +50,13 @@ def main():
     message_data = []
     while True:
         message = input()
-        number_of_chars = int(input())
+        number_of_chars = input()
         if message == "Over!":
             break
         else:
+            number_of_chars = int(number_of_chars)
             message = check_input(message, number_of_chars)
-            if message:
+            if message is not None:
                 message_data.append(message)
 
     print_output(message_data)
