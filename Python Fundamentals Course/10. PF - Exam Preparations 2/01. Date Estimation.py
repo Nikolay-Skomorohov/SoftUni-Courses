24643a440d268ec388ae565503b8400e0fc76cba
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2019-09-05 21:17:44 +0300

    Add partial exercise solution.

--- Python Fundamentals Course/10. PF - Exam Preparations 2/01. Date Estimation.py
+++ Python Fundamentals Course/10. PF - Exam Preparations 2/01. Date Estimation.py
@@ -8,4 +8,20 @@ year_input = read_input[0]
 month_input = read_input[1]
 day_input = read_input[2]
 
-
+if year_input <= year_today:
+    if month_input <= month_today:
+        if day_input < day_today:
+            print('Passed')
+        elif day_input == day_today:
+            print('1 day left')
+        else:
+            print(f"{day_today - day_input}")
+    else:
+        days_difference = day_input - day_today
+        months_difference = month_today -
+        print(f"{(months_difference * 30) + days_difference} days left")
+else:
+    days_difference = day_today - day_input
+    months_difference = month_today - month_input
+    years_difference = year_today - y
+    print(f"{(months_difference * 30) + days_difference} days left")
