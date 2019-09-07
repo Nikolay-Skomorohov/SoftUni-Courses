693b03c5739c3b8420da7efe84036912b257e4d9
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2019-09-07 13:51:17 +0300

    Add full exercise solution.

--- Python Fundamentals Course/10. PF - Exam Preparations 2/01. Date Estimation.py
+++ Python Fundamentals Course/10. PF - Exam Preparations 2/01. Date Estimation.py
@@ -1,3 +1,17 @@
+data_dict = {"1": 31,
+             "2": 28,
+             "22": 29,
+             "3": 31,
+             "4": 30,
+             "5": 31,
+             "6": 30,
+             "7": 31,
+             "8": 31,
+             "9": 30,
+             "10": 31,
+             "11": 30,
+             "12": 31}
+
 read_input = list(map(int, input().split('-')))
 
 year_today = 2018
@@ -8,20 +22,38 @@ year_input = read_input[0]
 month_input = read_input[1]
 day_input = read_input[2]
 
-if year_input <= year_today:
-    if month_input <= month_today:
-        if day_input < day_today:
-            print('Passed')
-        elif day_input == day_today:
-            print('1 day left')
-        else:
-            print(f"{day_today - day_input}")
+current_data = 26
+for year in range(year_today):
+    if year % 4 == 0 and not year % 100 == 0:
+        current_data += 366
+    elif year % 100 == 0 and not year % 400 == 0:
+        current_data += 365
+    elif year % 400 == 0:
+        current_data += 366
     else:
-        days_difference = day_input - day_today
-        months_difference = month_today -
-        print(f"{(months_difference * 30) + days_difference} days left")
+        current_data += 365
+
+for month in range(1, month_today):
+    current_data += data_dict[str(month)]
+
+other_data = int(day_input)
+for year in range(0, year_input):
+    if year % 4 == 0 and not year % 100 == 0:
+        other_data += 366
+    elif year % 100 == 0 and not year % 400 == 0:
+        other_data += 365
+    elif year % 400 == 0:
+        other_data += 366
+    else:
+        other_data += 365
+
+for month in range(1, month_input):
+    other_data += data_dict[str(month)]
+
+result = other_data - current_data
+if result == 0:
+    print("Today date")
+elif result < 0:
+    print("Passed")
 else:
-    days_difference = day_today - day_input
-    months_difference = month_today - month_input
-    years_difference = year_today - y
-    print(f"{(months_difference * 30) + days_difference} days left")
+    print(f"{result + 1} days left")
