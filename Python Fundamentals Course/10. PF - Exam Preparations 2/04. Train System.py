9cc5bdfe2eb9a74369edd0b10123e82adc44925e
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2019-09-12 23:21:57 +0300

    Add partial exercise solution.

--- Python Fundamentals Course/10. PF - Exam Preparations 2/04. Train System.py
+++ Python Fundamentals Course/10. PF - Exam Preparations 2/04. Train System.py
@@ -21,12 +21,6 @@ class Card:
         self.valid = None
         self.owner = self.first_name + " " + self.last_name
 
-    def check_card_validity(self):
-        if sum(int(x) for x in str(self.card_num)) % 7 == 0:
-            self.valid = True
-        else:
-            self.valid = False
-
 
 class Ticket:
     def __init__(self, first_name: str, last_name: str, destination: str, card_num: int):
@@ -39,14 +33,32 @@ class Ticket:
         self.price = sum(ord(x) for x in self.destination) / 100
 
     def __str__(self):
-        return f"{self.owner}:\n" \
-               f"--{self.destination}: {self.price:.2f}\n" \
-               f"--{self.destination}: {self.price / 2:.2f} (using card {self.use_card}"
+        if self.use_card is not True:
+            return f"{self.owner}:\n" \
+               f"--{self.destination}: {self.price:.2f}"
+        else:
+            return f"{self.owner}:\n" \
+                   f"--{self.destination}: {self.price / 2:.2f} (using card {self.use_card}"
 
 
-def print_result(tickets_list: list):
-    for ticket in tickets_list:
-        print(ticket)
+def print_result(people_list: list):
+    for person in people_list:
+        if len(person.tickets) < 0:
+            for ticket in person.tickets:
+                print(ticket)
+
+
+def check_card_validity(number: int, people_list: list):
+    if sum(int(x) for x in str(number)) % 7 == 0:
+        if all(x for x in people_list for card in x.cards if card != number):
+            print(f"issuing card {number}")
+            return True
+        else:
+            print(f"card {number} already exists for another passenger!")
+            return False
+    else:
+        print(f"card {number} is not valid!")
+        return False
 
 
 def main():
@@ -57,11 +69,12 @@ def main():
     for card in range(card_info_num):
         new_info = input().split()
         new_card = Card(new_info[0], new_info[1], int(new_info[2]))
+        new_card.valid = True
         cards_list.append(new_card)
-        if all(x.full_name != new_info[0] + " " + new_info[1] for x in people_list):
-            new_person = Person(new_info[0], new_info[1])
-            people_list.append(new_person)
-            new_person.cards.append(new_card)
+        # if not all(x.full_name != (new_info[0] + " " + new_info[1]) for x in people_list):
+        new_person = Person(new_info[0], new_info[1])
+        people_list.append(new_person)
+        new_person.cards.append(new_card)
 
     while True:
         command = input()
@@ -72,14 +85,16 @@ def main():
             new_ticket = Ticket(command[1], command[2], command[3], int(command[4]))
             tickets_list.append(new_ticket)
             if any(x for x in people_list for card in x.cards if card == new_ticket.card_num):
-                pass
-            #
+                new_ticket.use_card = True
+            else:
+                if check_card_validity(new_ticket.card_num, people_list):
+                    new_ticket.use_card = True
 
             if new_ticket.owner in [x.full_name for x in people_list]:
                 new_owner = next(x for x in people_list if x.full_name == new_ticket.owner)
                 new_owner.tickets.append(new_ticket)
 
-    print_result(tickets_list)
+    print_result(people_list)
 
 
 if __name__ == "__main__":
