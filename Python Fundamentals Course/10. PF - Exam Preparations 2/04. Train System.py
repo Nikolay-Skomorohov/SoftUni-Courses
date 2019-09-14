6e9458ef7c3e5c0988885c7f69869dd1bebb3b80
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2019-09-14 12:07:53 +0300

    Add partial exercise solution.

--- Python Fundamentals Course/10. PF - Exam Preparations 2/04. Train System.py
+++ Python Fundamentals Course/10. PF - Exam Preparations 2/04. Train System.py
@@ -3,114 +3,116 @@ class Person:
         self.first_name = first_name
         self.last_name = last_name
         self.full_name = self.first_name + " " + self.last_name
-        self.cards = []
-        self.tickets = []
-        self.total_cost = 0
+        self.personal_cards = []
+        self.personal_tickets = []
+        self.total_tickets_cost = 0
 
-    def check_for_card(self, input_number: int):
-        if any(x.card_num == input_number for x in self.cards):
-            return True
-        else:
-            return False
-
-    def calculate_total(self):
-        for ticket in self.tickets:
-            if ticket.use_card:
-                self.total_cost += (ticket.price / 2)
+    def calculate_total_cost(self):
+        for ticket in self.personal_tickets:
+            if ticket.uses_valid_card:
+                self.total_tickets_cost += (ticket.ticket_price / 2)
             else:
-                self.total_cost += ticket.price
+                self.total_tickets_cost += ticket.ticket_price
 
 
 class Card:
-    def __init__(self, first_name: str, last_name: str, card_num: str):
+    def __init__(self, first_name: str, last_name: str, card_number: str):
         self.first_name = first_name
         self.last_name = last_name
-        self.card_num = card_num
-        self.valid = None
-        self.owner = self.first_name + " " + self.last_name
+        self.full_name = self.first_name + " " + self.last_name
+        self.card_number = card_number
 
 
 class Ticket:
-    def __init__(self, first_name: str, last_name: str, destination: str, card_num: str):
+    def __init__(self, first_name: str, last_name: str, destination: str, card_number_input: str):
         self.first_name = first_name
         self.last_name = last_name
+        self.full_name = self.first_name + " " + self.last_name
         self.destination = destination
-        self.owner = self.first_name + " " + self.last_name
-        self.card_num = card_num
-        self.use_card = None
-        self.price = sum(ord(x) for x in self.destination) / 100
+        self.card_number_input = card_number_input
+        self.uses_valid_card = False
+        self.ticket_price = sum(ord(x) for x in self.destination) / 100
 
     def __str__(self):
-        if self.use_card is not True:
-            return f"--{self.destination}: {self.price:.2f}lv"
+        if self.uses_valid_card:
+            print(f"--{self.destination}: {self.ticket_price / 2:.2f}lv (using card {self.card_number_input})")
         else:
-            return f"--{self.destination}: {self.price / 2:.2f}lv (using card {self.card_num})"
+            print(f"--{self.destination}: {self.ticket_price:.2f}lv")
 
 
-def print_result(people_list: list):
-    for person in people_list:
-        person.calculate_total()
-    for person in sorted(people_list, key=lambda x: -x.total_cost):
-        if len(person.tickets) > 0:
-            print(f"{person.full_name}:")
-            for ticket in sorted(person.tickets, key=lambda x: -x.price):
-                print(ticket)
-            print(f"total: {person.total_cost:.2f}lv")
+def create_new_person(first_name: str, last_name: str):
+    return Person(first_name, last_name)
 
 
-def check_card_validity(number: str, people_list: list):
-    if sum(int(x) for x in number) % 7 == 0:
-        if not any(x for x in people_list for card in x.cards if card.card_num == number):
-            print(f"issuing card {number}")
-            return True
-        else:
-            if not any(x for x in people_list for ticket in x.tickets if ticket.card_num == number):
-                return True
-            else:
-                print(f"card {number} already exists for another passenger!")
-                return False
+def create_new_card(first_name: str, last_name: str, card_number: str):
+    return Card(first_name, last_name, card_number)
+
+
+def create_new_ticket(first_name: str, last_name: str, destination: str, card_number_input: str):
+    return Ticket(first_name, last_name, destination, card_number_input)
+
+
+def check_card_info(persons_list: list, card_to_check: str, owner_to_check: str, new_ticket: Ticket):
+    person = next(x for x in persons_list if x.full_name == owner_to_check)
+    if card_to_check in [x.full_name for x in person.personal_cards]:
+        new_ticket.uses_valid_card = True
     else:
-        print(f"card {number} is not valid!")
-        return False
+        if sum(int(x) for x in card_to_check) % 7 == 0:
+            if card_to_check in [x for x in persons_list for card in x.personal_cards if x.full_name != owner_to_check]:
+                print(f"card {card_to_check} already exists for another passenger!")
+            else:
+                new_ticket.uses_valid_card = True
+                print(f"issuing card {card_to_check}")
+        else:
+            print(f"card {card_to_check} is not valid!")
+
+
+def initial_cards_input():
+    persons_list = []
+    number_of_cards = int(input())
+    for card in range(number_of_cards):
+        new_info = input().split()
+        new_person = create_new_person(new_info[0], new_info[1])
+        new_card = create_new_card(new_info[0], new_info[1], new_info[2])
+        new_person.personal_cards.append(new_card)
+        persons_list.append(new_person)
+    return persons_list
+
+
+def input_for_tickets(persons_list: list):
+    while True:
+        input_info = input().split()
+        if input_info[0] == "time":
+            break
+        else:
+            new_ticket = create_new_ticket(input_info[1], input_info[2], input_info[3], input_info[4])
+            if any(x for x in persons_list if x.full_name == new_ticket.full_name):
+                existing_person = next(x for x in persons_list if x.full_name == new_ticket.full_name)
+                existing_person.personal_tickets.append(new_ticket)
+            else:
+                new_person = create_new_person(input_info[1], input_info[2])
+                persons_list.append(new_person)
+                new_person.personal_tickets.append(new_ticket)
+
+            card_to_check = input_info[4]
+            owner_to_check = input_info[1] + " " + input_info[2]
+            check_card_info(persons_list, card_to_check, owner_to_check, new_ticket)
+
+    return persons_list
+
+
+def print_result(persons_list: list):
+    for person in persons_list:
+        person.calculate_total_cost()
+    for person in sorted(persons_list, key=lambda x: -x.total_tickets_cost):
+        print(f"{person.full_name}:")
+        for ticket in sorted(person.personal_tickets, key=lambda x: -x.ticket_price):
+            print(ticket)
 
 
 def main():
-    people_list = []
-    cards_list = []
-    tickets_list = []
-    card_info_num = int(input())
-    for card in range(card_info_num):
-        new_info = input().split()
-        new_card = Card(new_info[0], new_info[1], new_info[2])
-        new_card.valid = True
-        cards_list.append(new_card)
-        if not any(x.full_name == (new_info[0] + " " + new_info[1]) for x in people_list):
-            new_person = Person(new_info[0], new_info[1])
-            people_list.append(new_person)
-            new_person.cards.append(new_card)
-
-    while True:
-        command = input()
-        if command == "time to leave!":
-            break
-        else:
-            command = command.split()
-            new_ticket = Ticket(command[1], command[2], command[3], command[4])
-            tickets_list.append(new_ticket)
-            if not any(x.full_name == (command[1] + " " + command[2]) for x in people_list):
-                new_person = Person(command[1], command[2])
-                people_list.append(new_person)
-            if any(x for x in people_list for card in x.cards if card == new_ticket.card_num):
-                new_ticket.use_card = True
-            else:
-                if check_card_validity(new_ticket.card_num, people_list):
-                    new_ticket.use_card = True
-
-            if new_ticket.owner in [x.full_name for x in people_list]:
-                new_owner = next(x for x in people_list if x.full_name == new_ticket.owner)
-                new_owner.tickets.append(new_ticket)
-
-    print_result(people_list)
+    persons_list = input_for_tickets(initial_cards_input())
+    print_result(persons_list)
 
 
 if __name__ == "__main__":
