5090b382e9c925bb1f7b695e750a4ec7fd997166
Nikolay-Skomorohov <nikolay_skomorohov@protonmail.com>
2020-02-22 21:49:12 +0200

    Remove redundant code.

--- Python Advanced Module/Python Advanced Course/11. Exam Preparation 2/03. Magic Triangle.py
+++ Python Advanced Module/Python Advanced Course/11. Exam Preparation 2/03. Magic Triangle.py
@@ -3,11 +3,10 @@ def get_magic_triangle(n):
     for line in range(2, n):
         new_row = [1]
         for cell in range(1, line):
-            num_index = [line, cell - 1]
             new_row.append(result[line - 1][cell] + result[line - 1][cell - 1])
         new_row.append(1)
         result.append(new_row)
     return result
 
 
-print(get_magic_triangle(7))
+print(get_magic_triangle(5))
