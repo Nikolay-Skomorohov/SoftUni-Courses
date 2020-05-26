74c2b182ed72e5796eadf8caeb4aba4e46cc0dca
Nikolay-Skomorohov <nikolay_skomorohov@protonmail.com>
2020-05-26 22:32:49 +0300

    Added excercise solution.

---
+++ JavaScript Advanced Module/JavaScript Advanced Course/02. Arrays/Lab/01. Sum First Last.js
@@ -0,0 +1,8 @@
+function solve(aList) {
+    const num1 = Number(aList[0])
+    const num2 = Number(aList[aList.length - 1])
+    const result = num1 + num2
+    console.log(result)
+}
+
+solve(['20', '30', '40'])
