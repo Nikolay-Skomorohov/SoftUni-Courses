86a5114f72d48e29c65b0a691cd0f5e2e6583f52
Nikolay-Skomorohov <nikolay_skomorohov@protonmail.com>
2020-06-08 00:20:20 +0300

    Added excercise solution.

--- JavaScript Advanced Module/JavaScript Advanced Course/04. DOM/Excercises/01. Subtraction/subtract.js
+++ JavaScript Advanced Module/JavaScript Advanced Course/04. DOM/Excercises/01. Subtraction/subtract.js
@@ -1,3 +1,7 @@
 function subtract() {
-    console.log('TODO:...');
+    const getFirstNumber = Number(document.getElementById("firstNumber").value);
+    const getSecondNumber = Number(document.getElementById("secondNumber").value);
+    const result = (getFirstNumber - getSecondNumber);
+    const resultDiv = document.getElementById("result");
+    resultDiv.innerText = result;
 }
