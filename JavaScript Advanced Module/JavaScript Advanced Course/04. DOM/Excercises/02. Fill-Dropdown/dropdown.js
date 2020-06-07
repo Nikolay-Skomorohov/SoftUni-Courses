d7b03861b3c4356bd9737e10996c341a8345c523
Nikolay-Skomorohov <nikolay_skomorohov@protonmail.com>
2020-06-08 00:38:19 +0300

    Added excercise solution.

--- JavaScript Advanced Module/JavaScript Advanced Course/04. DOM/Excercises/02. Fill-Dropdown/dropdown.js
+++ JavaScript Advanced Module/JavaScript Advanced Course/04. DOM/Excercises/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    const textInputField = document.getElementById('newItemText').value;
+    const valueInputField = document.getElementById('newItemValue').value;
+    const selectMenu = document.getElementById('menu');
+    const newOptionCreate = document.createElement('option')
+    newOptionCreate.textContent = textInputField;
+    newOptionCreate.value = valueInputField;
+    selectMenu.appendChild(newOptionCreate);
+    document.getElementById('newItemText').value = "";
+    document.getElementById('newItemValue').value = "";
 }
