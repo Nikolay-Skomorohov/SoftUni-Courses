99dfcc7986105fe1d256f717ea53703d3956597b
Nikolay-Skomorohov <nikolay_skomorohov@protonmail.com>
2020-06-08 23:34:20 +0300

    Added excercise solution.

--- JavaScript Advanced Module/JavaScript Advanced Course/04. DOM/Excercises/05. Chat-Room/solution.js
+++ JavaScript Advanced Module/JavaScript Advanced Course/04. DOM/Excercises/05. Chat-Room/solution.js
@@ -1,5 +1,15 @@
 function solve() {
-   //TODO...
+   const sendButtonTarget = document.getElementById('send');
+   sendButtonTarget.addEventListener('click', function() {
+      const allMessageTagElem = document.getElementById('chat_messages');
+      const chatInputElem = document.getElementById('chat_input');
+      const chatInputText = chatInputElem.value;
+      const newMessageElem = document.createElement('div');
+      newMessageElem.className = "message my-message";
+      newMessageElem.textContent = chatInputText;
+      document.getElementById('chat_messages').appendChild(newMessageElem);
+      chatInputElem.value = "";
+   })
 }
 
 
