76e5de10f863613d3b3eb709a4f4e2c4d20c954c
Nikolay-Skomorohov <nikolay_skomorohov@protonmail.com>
2020-06-10 22:27:19 +0300

    Added excercise solution.

--- JavaScript Advanced Module/JavaScript Advanced Course/05. DOM Manipulations/Excercises/01. Sections/script.js
+++ JavaScript Advanced Module/JavaScript Advanced Course/05. DOM Manipulations/Excercises/01. Sections/script.js
@@ -1,3 +1,14 @@
 function create(words) {
-   console.log('TODO:...');
+   const parentDiv = document.getElementById('content');
+
+   for (let item = 0; item < words.length; item++) {
+      parentDiv.appendChild(document.createElement('div'));
+      const newDiv = document.querySelectorAll('#content > div')[item];
+      newDiv.appendChild(document.createElement('p')).textContent = words[item];
+      const newP = newDiv.firstChild;
+      newP.style.display = 'none';
+      newDiv.addEventListener('click', function() {
+         newP.style.display = 'block';
+      });
+   }
 }
