74bda757a4d2db2091d498f2117136e8e3af6a48
Nikolay-Skomorohov <nikolay_skomorohov@protonmail.com>
2020-06-12 22:06:07 +0300

    Added excercise solution.

--- JavaScript Advanced Module/JavaScript Advanced Course/05. DOM Manipulations/Excercises/06. Table-Search-Engine/solution.js
+++ JavaScript Advanced Module/JavaScript Advanced Course/05. DOM Manipulations/Excercises/06. Table-Search-Engine/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-   //TODO...
+   const searchButton = document.getElementById('searchBtn');
+   const searchInput = document.getElementById('searchField');
+   const tableBody = document.querySelector('tbody');
+   const tableRows = Array.from(tableBody.querySelectorAll('td'));
+   const tableTRs = Array.from(tableBody.querySelectorAll('tr'));
+
+   searchButton.addEventListener('click', function() {
+      tableTRs.forEach(element => {
+         element.classList.remove('select');
+      });
+      for (let elem of tableRows) {
+         if (elem.textContent.toLowerCase() === searchInput.value.toLowerCase() || 
+            elem.textContent.toLowerCase().includes(searchInput.value.toLowerCase())) {
+            elem.parentElement.classList.add('select');
+         }
+      }
+      searchInput.value = "";
+   });
 }
