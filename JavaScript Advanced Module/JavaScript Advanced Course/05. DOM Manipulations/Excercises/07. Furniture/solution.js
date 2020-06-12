31ff4b778b9a388d6f481b3edf5b3a251d3ae13a
Nikolay-Skomorohov <nikolay_skomorohov@protonmail.com>
2020-06-13 02:51:42 +0300

    Added  excercise solution.

--- JavaScript Advanced Module/JavaScript Advanced Course/05. DOM Manipulations/Excercises/07. Furniture/solution.js
+++ JavaScript Advanced Module/JavaScript Advanced Course/05. DOM Manipulations/Excercises/07. Furniture/solution.js
@@ -1,34 +1,53 @@
 function solve() {
     const mainDiv = document.getElementById('exercise');
     const inputField = mainDiv.getElementsByTagName('textarea')[0];
-    const inputData = (inputField.value)
-    inputData.slice(1, -1)
-    inputData.split(', ');
+    const inputDataList = JSON.parse(inputField.value);
     const generateButton = mainDiv.getElementsByTagName('button')[0];
+    const buyButton = mainDiv.getElementsByTagName('button')[1];
+    const buyField = mainDiv.getElementsByTagName('textarea')[1];
     const tableBody = document.querySelector('tbody');
-    const newRow = document.createElement('tr');
-    const objectsLength = inputData.length;
-    let currentObjIndex = 0;
-    tableBody.appendChild(newRow);
 
     generateButton.addEventListener('click', function() {
-        const currentObj = JSON.parse(inputData[currentObjIndex]);
-        const newRow = document.createElement('tr');
-        tableBody.appendChild(newRow);
-        const lastRow = tableBody.querySelector('tr:last-child');
-        const objKeys = Object.keys(currentObj);
-        for (let i = 0; i < 5; i++) {
-            
-            let rowContent = "<td>"
-            if (i === 1) {
-                rowContent += `<img scr=${currentObj[objKeys[i]]}>`
-            }
-            else {
-                rowContent += `<p>${currentObj[objKeys[i]]}<\/p>`;
-            }
-            rowContent += '<\/td>';
+        for (let obj of inputDataList) {
+            const newRow = document.createElement('tr');
+            tableBody.appendChild(newRow);
+            const newestRow = tableBody.lastChild;
+            let rowContent = "";
+            rowContent += `<td><img src=${obj['img']}><\/td>` +
+                        `<td><p>${obj['name']}<\/p><\/td>` +
+                        `<td><p>${obj['price']}<\/p><\/td>` +
+                        `<td><p>${obj['decFactor']}<\/p><\/td>` +
+                        `<td><input type="checkbox"\/><\/td>`;
+            newestRow.innerHTML = rowContent;
         }
-        lastRow.innerHTML(rowContent);
+    })
+
+    tableBody.addEventListener('click', function(e) {
+        eventTarget = e.target;
+        if (eventTarget.tagName === "INPUT") {
+            eventTarget.checked ? eventTarget.checked = true : eventTarget.checked = false;
+        }
+    })
+
+    buyButton.addEventListener('click', function(e) {
+        const checkMarksElems = Array.from(document.querySelectorAll('td > input'));
+        eventTarget = e.target;
+        if (eventTarget.tagName === "BUTTON" && eventTarget.textContent === "Buy") {
+            let itemNames = [];
+            let itemsPrice = 0;
+            let itemsDecor = [];
+            for (let item = 0; item < checkMarksElems.length; item++) {
+                if (checkMarksElems[item].checked === true) {
+                    const parentTDs = checkMarksElems[item].parentElement.parentElement.getElementsByTagName('td');
+                    itemNames.push(parentTDs[1].textContent);
+                    itemsPrice += +parentTDs[2].textContent;
+                    itemsDecor.push(+parentTDs[3].textContent);
+                }
+            }
+            itemsDecorAverage = itemsDecor.reduce((a, b) => a + b, 0) / itemsDecor.length;
+            buyField.value = ("Bought furniture: " + itemNames.join(", ") + "\n" 
+                                    + "Total price: " + itemsPrice.toFixed(2) + "\n"
+                                    + "Average decoration factor: " + itemsDecorAverage);
+        }
     })
 }
-
