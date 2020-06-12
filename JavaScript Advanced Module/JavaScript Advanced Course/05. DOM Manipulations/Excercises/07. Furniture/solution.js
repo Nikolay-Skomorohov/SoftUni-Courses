0f9a57a8929730b298bd8c4b8a3af6f7fada99c0
Nikolay-Skomorohov <nikolay_skomorohov@protonmail.com>
2020-06-12 23:53:57 +0300

    Added partial excercise solution.

--- JavaScript Advanced Module/JavaScript Advanced Course/05. DOM Manipulations/Excercises/07. Furniture/solution.js
+++ JavaScript Advanced Module/JavaScript Advanced Course/05. DOM Manipulations/Excercises/07. Furniture/solution.js
@@ -1,4 +1,34 @@
 function solve() {
+    const mainDiv = document.getElementById('exercise');
+    const inputField = mainDiv.getElementsByTagName('textarea')[0];
+    const inputData = (inputField.value)
+    inputData.slice(1, -1)
+    inputData.split(', ');
+    const generateButton = mainDiv.getElementsByTagName('button')[0];
+    const tableBody = document.querySelector('tbody');
+    const newRow = document.createElement('tr');
+    const objectsLength = inputData.length;
+    let currentObjIndex = 0;
+    tableBody.appendChild(newRow);
 
-  //TODO...
+    generateButton.addEventListener('click', function() {
+        const currentObj = JSON.parse(inputData[currentObjIndex]);
+        const newRow = document.createElement('tr');
+        tableBody.appendChild(newRow);
+        const lastRow = tableBody.querySelector('tr:last-child');
+        const objKeys = Object.keys(currentObj);
+        for (let i = 0; i < 5; i++) {
+            
+            let rowContent = "<td>"
+            if (i === 1) {
+                rowContent += `<img scr=${currentObj[objKeys[i]]}>`
+            }
+            else {
+                rowContent += `<p>${currentObj[objKeys[i]]}<\/p>`;
+            }
+            rowContent += '<\/td>';
+        }
+        lastRow.innerHTML(rowContent);
+    })
 }
+
