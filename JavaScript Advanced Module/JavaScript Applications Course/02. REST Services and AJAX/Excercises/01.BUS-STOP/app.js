51f7bd2136fa4afd161f3b445a4c965606688b4a
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2020-07-07 23:49:06 +0300

    Added partial excercise solution.

--- JavaScript Advanced Module/JavaScript Applications Course/02. REST Services and AJAX/Excercises/01.BUS-STOP/app.js
+++ JavaScript Advanced Module/JavaScript Applications Course/02. REST Services and AJAX/Excercises/01.BUS-STOP/app.js
@@ -1,3 +1,29 @@
 function getInfo() {
-    console.log("TODO...");
+    const stopIDtextEl = document.getElementById('stopId');
+    const stopIDbuttonEl = document.getElementById('submit');
+    const stopNameEl = document.getElementById('stopName');
+    const stopListEl = document.getElementById('buses');
+
+    stopIDbuttonEl.addEventListener('click', function checkBusStop() {
+        if (stopIDtextEl.value !== '') {
+            const inputText = stopIDtextEl.value;
+            const incomingData = fetch(`https://judgetests.firebaseio.com/businfo/${inputText}.json`)
+                .then(function(response) {
+                    if (response.status === 200) {
+                        return response.json()
+                    }
+                    else {
+                        return stopNameEl.textContent = 'Error';
+                    }})
+                .then(function (r) {
+                    return r;
+                })
+            stopNameEl.textContent = incomingData.name;
+            for (let bus of Object.keys(incomingData.buses)) {
+                let newLi = document.createElement('li');
+                newLi.innerText = bus;
+                stopListEl.appendChild(newLi);
+            }
+        }
+    });
 }
