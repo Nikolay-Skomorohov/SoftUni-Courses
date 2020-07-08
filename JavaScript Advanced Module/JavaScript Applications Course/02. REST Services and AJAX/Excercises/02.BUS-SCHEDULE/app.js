778ad77c4e3220f9ac977d91180ec3c066c15c9f
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2020-07-08 23:15:31 +0300

    Added full excercise solution.

--- JavaScript Advanced Module/JavaScript Applications Course/02. REST Services and AJAX/Excercises/02.BUS-SCHEDULE/app.js
+++ JavaScript Advanced Module/JavaScript Applications Course/02. REST Services and AJAX/Excercises/02.BUS-SCHEDULE/app.js
@@ -1,10 +1,36 @@
 function solve() {
+    const departButtonElem = document.getElementById('depart');
+    const arriveButtonElem = document.getElementById('arrive');
+    const textInfoElem = document.querySelector('#info .info');
+    let currentBusStop = 'depot';
+    let nextBusStop;
 
     function depart() {
-        console.log('Depart TODO...');
+        fetch(`https://judgetests.firebaseio.com/schedule/${currentBusStop}.json`)
+            .then(function(response) {
+                if (response.status !== 200) {
+                    textInfoElem.value = 'Error';
+                    departButtonElem.setAttribute('disabled', 'true');
+                    arriveButtonElem.setAttribute('disabled', 'true');
+                }
+                else {
+                    return response.json();
+                }
+            })
+            .then(function(data) {
+                textInfoElem.textContent = `Next stop ${data.name}`;
+                currentBusStop = data.name;
+                departButtonElem.setAttribute('disabled', 'true');
+                arriveButtonElem.removeAttribute('disabled');
+                nextBusStop = data.next;
+            })
     }
 
     function arrive() {
+        textInfoElem.textContent = `Arriving at ${currentBusStop}`;
+        departButtonElem.removeAttribute('disabled');
+        arriveButtonElem.setAttribute('disabled', 'true');
+        currentBusStop = nextBusStop;
         console.log('Arrive TODO...');
     }
 
