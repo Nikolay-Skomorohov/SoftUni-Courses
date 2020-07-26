f4b7490f87bfbfb53b1af99b2d27e6c5fe6c39a8
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2020-07-26 21:14:09 +0300

    Added exercise solution.

--- JavaScript Advanced Module/JavaScript Applications Course/05. Templating/Exercises/02. HTTP Status Cats/template.js
+++ JavaScript Advanced Module/JavaScript Applications Course/05. Templating/Exercises/02. HTTP Status Cats/template.js
@@ -1,8 +1,21 @@
-(() => {
-     renderCatTemplate();
+window.addEventListener('load', async function () {
+     const mainElem = document.getElementById('allCats');
+     Handlebars.registerPartial('cat', await (await fetch('./templates/status_cat.hbs')).text());
+     const getMainTemplate = await (await fetch('./templates/main_template.hbs')).text();
+     const mainTemplateFn = Handlebars.compile(getMainTemplate);
+     mainElem.innerHTML = mainTemplateFn({ cats });
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
+     mainElem.addEventListener('click', function(e) {
+          const target = e.target;
+
+          if (target.tagName === 'BUTTON') {
+             const eventElemDiv = target.parentNode.querySelector('.status');
+             if (eventElemDiv.style.display === 'none') {
+                  eventElemDiv.style.display = 'block';
+             }
+             else {
+                 eventElemDiv.style.display = 'none';
+             }
+          }
+     })
 })
