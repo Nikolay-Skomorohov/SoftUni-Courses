b15469e0197a50d76fc9f853e0a680dccc47ea8a
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2020-07-26 21:45:51 +0300

    Added exercise solution.

--- JavaScript Advanced Module/JavaScript Applications Course/05. Templating/Exercises/03. Popular Monkeys/monkeysTemplate.js
+++ JavaScript Advanced Module/JavaScript Applications Course/05. Templating/Exercises/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,21 @@
-$(() => {
-    // TODO
+window.addEventListener('load', async function() {
+    const sectionElem = document.getElementsByTagName('section')[0];
+    const partialMonkey = await (await fetch('./templates/monkey_template.hbs')).text();
+    Handlebars.registerPartial('monkey', partialMonkey);
+    const mainTemplate = await (await fetch('./templates/main_template.hbs')).text();
+    const mainTemplateFn = Handlebars.compile(mainTemplate);
+    sectionElem.innerHTML = mainTemplateFn({ monkeys });
+
+    sectionElem.addEventListener('click', function (e) {
+        const eventTarget = e.target;
+        if (eventTarget.tagName === 'BUTTON') {
+            const eventButtonElem = eventTarget.parentNode.getElementsByTagName('p')[0];
+            if (eventButtonElem.style.display === 'none') {
+                eventButtonElem.style.display = 'block';
+            }
+            else {
+                eventButtonElem.style.display = 'none'
+            }
+        }
+    })
 })
